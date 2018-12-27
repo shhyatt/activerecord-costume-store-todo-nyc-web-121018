c32e088bccbcbbc16f9cbee57c4fc09c35892ada
github username <github email address>
2018-12-27 12:55:47 -0500

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,23 @@
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.integer :size
+      t.string :image_url
+      t.timestamps 
+    end
+
+
+  end
+
+end
+
+
+
+
+
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
@@ -5,5 +25,5 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
